06e9d16ea446a7281b0970628fdbba74f13c09ed
damian-tworek <damian.tworek@spoton.com>
2022-12-19 21:59:43 +0100

    wip

--- app/src/main/java/com/example/decisionmaking/presentation/ui/Injector.kt
+++ app/src/main/java/com/example/decisionmaking/presentation/ui/Injector.kt
@@ -4,13 +4,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import com.example.decisionmaking.di.DataDI
-import com.example.decisionmaking.domain.di.DomainDI
 import com.example.decisionmaking.presentation.input.viewmodel.InputViewModel
 
 object Injector {
 
     private val dataDI = DataDI()
-    private val domainDI = DomainDI(dataDI)
 
     val InputViewModelFactory: ViewModelProvider.Factory = object : ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
